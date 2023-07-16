9b3c3049f364f7edef72b55eb68c553f8c51aec5
Marek Z <UnderscoreF17@gmail.com>
2023-07-16 20:04:46 +0200

    Add files via upload

--- notasecretfolder/loader.js
+++ notasecretfolder/loader.js
@@ -1,1 +1,1 @@
-document.getElementById("unselectable").innerHTML = "-.. .. ... -.-. --- .-. -.. / -- . ... ... .- --. ."
+document.getElementById("unselectable").innerHTML = "-... .. - .-.. -.-- / ....- -.... ... ...- -... .-- --- </div><div id='unselectable2'>first 4 letters are with caps.</div>";
